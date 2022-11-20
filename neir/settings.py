b3c943be83d4b50e2348b2fd718c56c02613a90a
unknown <babserv@gmail.com>
2022-11-21 01:09:51 +0500

    fix 1.8

--- neir/settings.py
+++ neir/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c2&r3eoh09hk1b#cka=e3_i+*tf86#2j4x331fdlr()9_ptqxy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
