8ba202c9ec71aeefbc4f38852855fd8b04fb7a41
sirbiprod <sirbiprod@gmail.com>
2022-11-22 03:12:19 +0500

    fix 2.19

--- pdf.py
+++ pdf.py
@@ -3,7 +3,7 @@ from PIL import Image
 from pdf2image import convert_from_path
 import pytesseract
 
-filePath = 'test.pdf'
+filePath = 'test2.pdf'
 doc = convert_from_path(filePath)
 path, fileName = os.path.split(filePath)
 fileBaseName, fileExtension = os.path.splitext(fileName)
